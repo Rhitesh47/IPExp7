0f2ab8ae996eebe630767d371988562e9f674778
Rhitesh47 <d2022.hitesh.rohra@ves.ac.in>
2024-10-15 22:39:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+      id: 1,
+      name: 'pink Velvet',
+      image: 'https://i.postimg.cc/h42rmkdh/images-1.jpg',
+      price: 399.95,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 2,
+      name: 'KitKat',
+      image: 'https://i.postimg.cc/NMY48YD2/images.jpg',
+      price: 299.98,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 3,
+      name: 'vanilla Chocohip',
+      image: 'https://i.postimg.cc/4yQjvBps/Vanilla-Choco-Chips-Cake.webp',
+      price: 1000.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 4,
+      name: 'fruit Vanilla',
+      image: 'https://i.postimg.cc/KzZPkfcn/230202-121111-214-057.jpg',
+      price: 999.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+  ]
+  const people = [
+    { id: 1, name: 'john' },
+    { id: 2, name: 'peter' },
+    { id: 3, name: 'susan' },
+    { id: 4, name: 'anna' },
+    { id: 5, name: 'emma' },
+  ]
+  module.exports = { products, people }
